06eb268077015c7b6ba38d2e874f910ae6bf615b
Timi Odusanya <timmycruz36@gmail.com>
2022-11-02 00:52:22 +0100

    Deploy linktree to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import profileImg from './profile_img.png';
+import github from "./github.png";
+import slack from "./slack.png";
+import zuriLogo from "./Zuri.Internship_Logo.png";
+import IFG from "./I4G.png";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="profile_section">
+        <img
+          id="profile__img"
+          className="profile__img"
+          src={profileImg}
+          alt="profile"
+        />
+        <h1 id="twitter" className="twitter">
+          Annette Black
+        </h1>
+        <h1 id="slack" hidden>
+          Timi Odusanya
+        </h1>
+      </div>
+
+      <div className="link_section">
+        <button className="link">
+          <a href="https://twitter.com/timi_odusanya">Twitter Link</a>
+        </button>
+        <button className="link" id="btn__zuri">
+          <a href="https://training.zuri.team/">Zuri Team</a>
+        </button>
+        <button className="link" id="books">
+          <a href="http://books.zuri.team/">Zuri Books</a>
+        </button>
+        <button className="link" id="book__python">
+          <a href="https://books.zuri.team/python-for-beginners?ref_id=TimiOdusanya">
+            Python Books
+          </a>
+        </button>
+        <button className="link" id="pitch">
+          <a href="https://background.zuri.team/">
+            Background Check for Coders
+          </a>
+        </button>
+        <button className="link">
+          <a href="https://books.zuri.team/design-rules">Design Books</a>
+        </button>
+        <div className="socials">
+          <img src={slack} id="" alt="" className="slack" />
+          <img src={github} id="" alt="" className="github" />
+        </div>
+      </div>
+
+      <hr />
+      <div className="footer">
+        <img src={zuriLogo} alt="zuri_logo" className="zuri_logo" />
+        <h1 className="footer_name">HNG Internship 9 Frontend Task</h1>
+        <img src={IFG} alt="ingressive" className="ifg" />
+      </div>
     </div>
   );
 }
